021cc2357fa590b31341574a5d1e73de221a0970
ymzhao <ymzhaobth@aliyun.com>
2020-12-01 17:09:34 +0800

    类名切换实现css动画

--- src/views/my-space/MySpace.vue
+++ src/views/my-space/MySpace.vue
@@ -2,17 +2,15 @@
 <template>
 	<div id="page-three">
 		<div class="container-1">
-			<div class="catalog-container" title="我的空间" @click="goTo('plus')">
-				<img style="width: 100%; height: 100%" src="./img/myspace.png"></img>
-			</div>
-			<div class="catalog-container" title="笔记" @click="goTo('notes')">
-				<img style="width: 100%; height: 100%" src="./img/notes.png"></img>
-			</div>
-			<div class="catalog-container" title="作品" @click="goTo('works')">
-				<img style="width: 100%; height: 100%" src="./img/works.png"></img>
-			</div>
-			<div class="catalog-container" title="插件" @click="goTo('plugs')">
-				<img style="width: 100%; height: 100%" src="./img/plugs.png"></img>
+			<div
+				v-for="catalog in catalogDatas"
+				:class="catalog.flag ? 'catalog-container' : 'catalog-container-1'"
+				:title="catalog.title"
+				@click="goTo(catalog.path)"
+				@mouseenter="catalog.flag = false"
+				@mouseleave="catalog.flag = true"
+			>
+				<img style="width: 100%; height: 100%" :src="catalog.imgSrc"></img>
 			</div>
 		</div>
 	</div>
@@ -22,7 +20,30 @@
 export default {
 	components: {},
 	data() {
-		return {};
+		return {
+			catalogDatas: [{
+					title: '我的空间',
+					path: 'plus',
+					imgSrc: require('./img/myspace.png'),
+					flag: true
+				},{
+					title: '笔记',
+					path: 'notes',
+					imgSrc: require('./img/notes.png'),
+					flag: true
+				},{
+					title: '作品',
+					path: 'works',
+					imgSrc: require('./img/works.png'),
+					flag: true
+				},{
+					title: '插件',
+					path: 'plugs',
+					imgSrc: require('./img/plugs.png'),
+					flag: true
+				}
+			]
+		};
 	},
 	mounted() {},
 	methods: {
@@ -38,14 +59,10 @@ export default {
 	0% {transform: translateX(0);}
 	100% {transform: translateX(40);}
 }
-/*@keyframes inblockAnimation {
-	0% {border-radius: 10px 0;}
-	100% {border-radius: 0 10px;}
+@keyframes test1 {
+	0% {transform: scale(1);}
+	100% {transform: scale(2);}
 }
-@keyframes outblockAnimation {
-	0% {border-radius: 0 10px;}
-	100% {border-radius: 10px 0;}
-}*/
 #page-three {
 	/*padding: 50px 80px;*/
 	background-color: rgba(0,0,0,0);
@@ -64,6 +81,14 @@ export default {
 	align-items: center;
 	justify-content: center;
 }
+@keyframes inblockAnimation {
+	0% {border-radius: 0 10px;transform: scale(1);}
+	100% {border-radius: 10px 0;transform: scale(1.05);}
+}
+@keyframes outblockAnimation {
+	0% {border-radius: 10px 0;transform: scale(1.05);}
+	100% {border-radius: 0 10px;transform: scale(1);}
+}
 .catalog-container {
 	cursor: pointer;
 	height: 80px;
@@ -74,6 +99,22 @@ export default {
     box-sizing: border-box;
 	background-color: #16a085;
 	border-radius: 10px 0;
-	/*animation: borderAnimation 2s 1 ease;*/
+	/* transition-duration: 0.5s; */
+	animation: outblockAnimation 0.3s 1 ease;
+	animation-fill-mode:forwards;
+}
+.catalog-container-1 {
+	cursor: pointer;
+	height: 80px;
+	width: 80px;
+	padding: 20px;
+	margin: 0 50px;
+    color: #fff;
+    box-sizing: border-box;
+	background-color: #16a085;
+	border-radius: 0 10px;
+	/* transition-duration: 0.5s; */
+	animation: inblockAnimation 0.3s 1 ease;
+	animation-fill-mode:forwards;
 }
 </style>

5f3e7f761feafcdd5d4121ee494f0a1e54dc29df
zymbth <ymzhaobth@gmail.com>
2023-03-06 23:53:55 +0800

    更新about封面

--- src/utils/common-methods.js
+++ src/utils/common-methods.js
@@ -12,3 +12,55 @@ export function copyToClipBoard(val) {
   inputEl.remove()
   return flag
 }
+
+// typeof link: https://juejin.cn/post/7000300249235357709#heading-3
+export function myTypeof(data) {
+  return data instanceof Element ? "element" :
+    Object.prototype.toString.call(data).replace(/\[object\s(.+)\]/, "$1").toLowerCase()
+}
+
+/**
+ * 根据模板获取对象 - 自用
+ * @param {object} tmpl 模板
+ * @param {object} obj 源对象
+ * @return {object}
+ */
+export function getObjectByTmpl(tmpl, obj) {
+  const res = {}
+  if(myTypeof(tmpl) !== 'object') return res
+  if(myTypeof(obj) !== 'object') obj = {}
+  // 遍历模板属性，从obj中获取对应值
+  Object.entries(tmpl).forEach(([k, v]) => {
+    /**
+     * 嵌套模板，值类型为对象。递归，根据子模板该对象值
+     * Example:
+     * tmpl: { ..., user: { name: '', age: '' } }
+     * obj: { ..., user: { name: 'Tom', age: 25, tel: 13311112222 } }
+     */
+    if(myTypeof(v) === 'object') res[k] = getObjectByTmpl(v, obj[k])
+    /**
+     * 嵌套模板，值类型为数组。递归，根据子模板获取该数组值
+     * Example:
+     * tmpl: { ..., addresses: [{ province: '', city: '', area: '', detail: '' }] }
+     * obj: { ..., addresses: [{},{},{}] }
+     */
+    else if(myTypeof(v) === 'array') {
+      res[k] = v.length && myTypeof(v[0]) === 'object'
+        ? (obj[k]?.map(p => getObjectByTmpl(v[0], p))??[]) : obj[k]
+    }
+    // 模板值为其它类型时，直接根据属性名从obj中获取对应值
+    else res[k] = obj[k]
+  })
+  return res
+}
+
+/**
+ * 根据模板获取对象数组 - 自用
+ * @param {object} tmpl 模板
+ * @param {array} arr 源数组
+ * @returns 
+ */
+export function getObjectArrByTmpl(tmpl, arr) {
+  return myTypeof(arr) === 'array' && arr.length
+    ? arr.map(p => getObjectByTmpl(tmpl, p)) : []
+}
--- src/views/about/index.vue
+++ src/views/about/index.vue
@@ -1,5 +1,32 @@
 <script setup>
+import { ref, reactive, onMounted } from 'vue';
+import { getObjectByTmpl } from '@/utils/common-methods'
 import ObserverWrap from './comp/observer-wrap.vue'
+import axios from 'axios'
+
+onMounted(() => {
+  fetchYiYan()
+})
+
+const tmpl = {
+  id: null,     // 一言标识
+  type: '',     // 类型. a - 动画, b - 漫画, c - 游戏, d - 文学, e - 原创, f - 来自网络, g - 其他, h - 影视, i - 诗词, j - 网易云, k - 哲学, l - 抖机灵
+  from: '',     // 一言的出处
+  from_who: '', // 一言的作者
+  hitokoto: '', // 一言正文
+  length: 0,    // 句子长度
+}
+const yiyan = reactive({}), loading = ref(false)
+const fetchYiYan = () => {
+  if(loading.value) return
+  loading.value = true
+  axios.get('https://v1.hitokoto.cn?c=d&c=i&c=k').then(res => {
+    Object.assign(yiyan, getObjectByTmpl(tmpl, res.data ?? {}))
+  }).finally(_ => {
+    console.log('finished')
+    loading.value = false
+  })
+}
 
 const careers = [{
   time: '2021/3/28',
@@ -42,16 +69,79 @@ const careers = [{
 }]
 </script>
 <template>
-  <div class="about-container">
-    <div style="background-color:aqua;height:100vh;"></div>
-    <h5 class="page-title">Career</h5>
-    <p class="page-desc">Education and working experiences</p>
-    <div class="time-line-wrap">
-      <ObserverWrap v-for="item in careers" :data="item"></ObserverWrap>
+  <div>
+    <div class="cover">
+      <div class="yiyan" v-show="yiyan.hitokoto">
+        <span class="yiyan-content">{{ yiyan.hitokoto }}</span>
+        <span v-show="yiyan.from_who" class="yiyan-author">———— {{ yiyan.from_who }}</span>
+        <svg
+          class="icon"
+          viewBox="0 0 1024 1024"
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          @click="fetchYiYan()"
+        >
+          <path fill="currentColor" d="M710.4 249.6l76.8-76.8c2.133-2.133 4.267-4.267 6.4-2.133 2.133 0 2.133 4.266 2.133 6.4v204.8c0 4.266-2.133 8.533-4.266 10.666s-6.4 4.267-10.667 4.267H576c-4.267 0-6.4 0-6.4-2.133s0-4.267 2.133-6.4l76.8-76.8C605.867 283.733 561.067 268.8 512 268.8c-44.8 0-85.333 10.667-121.6 32s-66.133 51.2-87.467 87.467-32 78.933-32 121.6H185.6c0-44.8 8.533-87.467 25.6-128 17.067-40.534 40.533-74.667 70.4-104.534s64-53.333 104.533-70.4c40.534-17.066 83.2-25.6 128-25.6 70.4 2.134 136.534 25.6 196.267 68.267zM755.2 512h85.333c0 44.8-8.533 87.467-25.6 128-17.066 40.533-40.533 74.667-70.4 104.533-29.866 29.867-64 53.334-104.533 70.4-40.533 17.067-83.2 25.6-128 25.6-72.533 0-138.667-21.333-198.4-66.133l-76.8 76.8c-2.133 2.133-4.267 4.267-6.4 2.133-2.133 0-2.133-4.266-2.133-6.4v-204.8c0-4.266 2.133-8.533 4.266-10.666 2.134-2.134 6.4-4.267 10.667-4.267H448c4.267 0 6.4 0 6.4 2.133s0 4.267-2.133 6.4L377.6 710.4c42.667 29.867 87.467 42.667 136.533 42.667 44.8 0 85.334-10.667 121.6-32 38.4-21.334 66.134-51.2 87.467-87.467s32-76.8 32-121.6z"/>
+        </svg>
+      </div>
+    </div>
+    <div class="about-container">
+      <h5 class="page-title">Career</h5>
+      <p class="page-desc">Education and working experiences</p>
+      <div class="time-line-wrap">
+        <ObserverWrap v-for="item in careers" :data="item"></ObserverWrap>
+      </div>
     </div>
   </div>
 </template>
 <style lang="scss" scoped>
+.cover {
+  height: clamp(400px, 100vh, 1000px);
+  background-image: url(../../assets/img/climbing-bg.jpg);
+  background-size: cover;
+  background-position: 0% 0%;
+  position: relative;
+
+  .yiyan {
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    color: #fff;
+    min-width: 260px;
+    max-width: 400px;
+    >span {
+      display: block;
+      line-height: 1.4em;
+    }
+    .yiyan-content {
+      font-size: 16px;
+      text-align: left;
+      text-indent: 2em;
+      text-shadow: 0px 0px 2px #00000066;
+    }
+    .yiyan-author {
+      font-size: 14px;
+      text-align: right;
+    }
+    .icon {
+      display: none;
+      position: absolute;
+      top: 0;
+      right: 0;
+      transform: translate(50%, -50%);
+      cursor: pointer;
+    }
+    &:hover .icon {
+      display: block;
+    }
+  }
+}
+.about-container {
+  width: 100%;
+  box-sizing: border-box;
+  overflow-x: hidden;
+}
 @media screen and (max-width:767px) {
   .about-container {padding: 20px 20px 20px 0;}
 }

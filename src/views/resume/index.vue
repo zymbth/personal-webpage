6636fe64b43b4dacd8bc83abe64bb706b21704ea
ymzhao <ymzhaobth@gmail.com>
2023-10-01 11:42:46 +0800

    upd url

--- src/views/resume/index.vue
+++ src/views/resume/index.vue
@@ -4,8 +4,8 @@
   <div class="resume">
     <div class="resume-page">Resume</div>
     <!-- <img src="http://ymzhao.work:7777/test.png" /> -->
-    <img src="http://127.0.0.1:7777/test.png" />
-    <iframe src="http://localhost/files/前端开发工程师_赵跃旻_瑞典布莱津理工大学_13148787915.pdf" width="800" height="1200"></iframe>
+    <img src="http://ymzhao.work/files/test.png" />
+    <iframe src="http://ymzhao.work/files/前端开发工程师_赵跃旻_瑞典布莱津理工大学_13148787915.pdf" width="800" height="1200"></iframe>
   </div>
 </template>
 <style lang="scss" scoped>

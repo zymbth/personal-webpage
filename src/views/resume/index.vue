0aeecb315617f8fc5a0711980f8ee84a2936d71f
ymzhao <ymzhaobth@gmail.com>
2023-10-01 11:02:50 +0800

    upd

--- src/views/resume/index.vue
+++ src/views/resume/index.vue
@@ -3,7 +3,7 @@
 <template>
   <div class="resume">
     <div class="resume-page">Resume</div>
-    <img src="http://127.0.0.1:7777/test.png" />
+    <img src="http://localhost:7777/test.png" />
     <!-- <iframe src="/resume-front-end" width="800" height="1200"></iframe> -->
   </div>
 </template>

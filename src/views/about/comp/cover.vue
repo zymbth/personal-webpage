4a62db30ea9b34d1dbda6363d86f73b90cbc6e10
ymzhao <ymzhaobth@gmail.com>
2023-10-23 11:51:28 +0800

    test

--- src/views/about/comp/cover.vue
+++ src/views/about/comp/cover.vue
@@ -50,7 +50,7 @@ const bgPosi = computed(() => -Math.floor(scrollTop.value / 2) + 'px')
 <style lang="scss" scoped>
 .cover {
   min-height: clamp(400px, 100vh, 1000px);
-  background-image: url(../../../assets/img/preload/climbing-bg.webp);
+  background-image: url(../../../assets/img/climbing-bg.webp);
   background-size: cover;
   background-position-x: 50%;
   background-position-y: v-bind(bgPosi);

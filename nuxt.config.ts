0494b724388295b2049a44360b2df9036c99769a
ymzhao <ymzhaobth@gmail.com>
2023-10-25 16:14:33 +0800

    echarts分包

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,45 @@
+// const CompressionWebpackPlugin = require('compression-webpack-plugin')
+// const productionGzipExtensions = ['js', 'css']
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  // build: {
-  //   transpile: [/echarts/],
-  // },
+  build: {
+    // transpile: [/echarts/],
+    analyze: {
+      analyzerMode: 'static'
+    },
+  },
   app: {
     head: {
-      // title: 'test'
       link: [
         { rel: 'icon', type: 'image/jpg', href: '/icon.jpg' }
       ]
+    }
+  },
+  // webpack: {
+  //   analyze: { analyzerMode: 'static' },
+  //   plugins: [
+  //     new CompressionWebpackPlugin({
+  //       filename: '[path].gz[query]',
+  //       algorithm: 'gzip',
+  //       test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+  //       threshold: 10240,
+  //       minRatio: 0.8,
+  //     })
+  //   ]
+  // }
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          // manualChunks: {
+          //   echarts: ['echarts'],
+          // }
+          manualChunks: (id) => {
+            if(id.includes('echarts')) return 'echarts'
+          } 
+        }
+      }
     }
   }
 })

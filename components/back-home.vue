36dfc4f68df89764a21a7594c15671ca06f7322d
ymzhao <ymzhaobth@gmail.com>
2023-10-30 12:03:49 +0800

    删除无用组件；修复打包后滚动出现的阻塞

--- components/back-home.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <NuxtLink to="/"><img src="~/assets/img/home.svg" alt="" style="height: 24px; vertical-align: middle" />Home</NuxtLink>
-</template>
--- components/jump-comp.jsx
+++
@@ -1,5 +0,0 @@
-import jumpIcon from '../assets/img/jump.svg'
-
-export default ({ path }) => {
-  return <NuxtLink to={'/'+path}><img src={jumpIcon} alt="jump" style="height: 20px; vertical-align: middle;" /></NuxtLink>
-}
